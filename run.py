20098d91a0b0561c86520b74712101021024167a
lewiEyasu <eyasulewi@gmail.com>
2025-02-12 17:17:41 +0300

    Improve error handling and output directory management in run.py
    - Create 'output' directory if it doesn't exist
    - Modify error handling to write error message to a text file
    - Rename output PowerPoint file to 'result.pptx'
    - Simplify output file path generation
    - Enhance script's robustness in handling file and directory operations

--- run.py
+++ run.py
@@ -16,6 +16,13 @@ logger = logging.getLogger(__name__)
 
 def main(): 
 
+    # Get the directory of the current script (run.py)
+    script_dir = Path(__file__).parent
+
+    # Define the path for the 'output' folder inside the script's directory
+    output_dir = script_dir / 'output'
+    output_dir.mkdir(exist_ok=True)  # Create 'output' directory if it doesn't exist
+
     url = os.environ.get('url', 'default_value')
     if url == 'default_value' or url == '':
         logger.error(f"Can't generate power point from the given url.)")
@@ -27,18 +34,13 @@ def main():
     try: 
         slides_data = convo2slide.pipeline()
     except:
-        raise "Can't generate slides from the given url"
+        output_file = str(output_dir / 'result.txt')
+        with open(output_file, 'w') as f:
+            f.write("Can't generate slides from the given url.")
+        raise Exception("Can't generate slides from the given url.")
     
-   
-   # Get the directory of the current script (run.py)
-    script_dir = Path(__file__).parent
-
-    # Define the path for the 'output' folder inside the script's directory
-    output_dir = script_dir / 'output'
-    output_dir.mkdir(exist_ok=True)  # Create 'output' directory if it doesn't exist
-
     # Define the file path within the 'output' directory
-    output_file = str(output_dir / 'result3.pptx')
+    output_file = str(output_dir / 'result.pptx')
 
    
     presentation = create_presentation(slides_data)
